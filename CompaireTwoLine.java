56e7843dc656f5b89330a0f051afb39fb89d27fe
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-09-02 20:23:44 +0530

    Create CompaireTwoLine.java

---
+++ CompaireTwoLine.java
@@ -0,0 +1,25 @@
+package Comparision;
+import java.util.Scanner;
+
+public class CompaireTwoLine {
+	public static void main(String[] args) {
+		System.out.println("This is the program length calculate");
+		 Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the first length1");
+		double length1 = sc.nextDouble();
+		System.out.println("Enter the second length2");	
+	    double length2 = sc.nextDouble();	
+	System.out.println("the length is :" +length1);
+	System.out.println("the length is :" +length2);
+	if (length1 == length2)
+	{
+		
+		System.out.println("both are equl");
+	}
+		else
+		{
+			System.out.println("both are not equl");
+	}
+	}
+
+}
